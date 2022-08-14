91c91efd945c495eca1a22a1974f55ae31ebea43
md.salehzadeh <md.salehzadeh@gmail.com>
2022-08-14 22:17:51 +0430

    some changes

---
+++ src/components/Progressbar/Index.svelte
@@ -0,0 +1,44 @@
+<script>
+	import classNames from 'classnames';
+
+	export let progress = '45';
+	export let size = 'h-2.5';
+	export let labelInside = false;
+	export let labelOutside = '';
+	export let color = 'primary';
+
+	const barColors = {
+		primary: 'bg-primary-600',
+		secondary: 'bg-secondary-600 dark:bg-secondary-300',
+		info: 'bg-info-600 dark:bg-info-500',
+		success: 'bg-success-600 dark:bg-success-500',
+		warning: 'bg-warning-400',
+		danger: 'bg-danger-600 dark:bg-danger-500',
+		purple: 'bg-purple-600 dark:bg-purple-500',
+		indigo: 'bg-indigo-600 dark:bg-indigo-500'
+	};
+
+	const barColor = barColors[color] ?? barColors.info;
+
+	const classes = classNames(barColor, size, 'rounded-full');
+</script>
+
+{#if labelOutside}
+	<div class="flex justify-between w-full mb-1">
+		<span class="text-base font-medium text-primary-700 dark:text-white">{labelOutside}</span>
+		<span class="text-sm font-medium text-primary-700 dark:text-white">{progress}%</span>
+	</div>
+{/if}
+
+<div class="w-full bg-gray-200 rounded-full {size} dark:bg-gray-700">
+	{#if labelInside}
+		<div
+			class="bg-primary-600 text-xs font-medium text-primary-100 text-center p-0.5 leading-none rounded-full"
+			style="width: {progress}%"
+		>
+			{progress}%
+		</div>
+	{:else}
+		<div class={classes} style="width: {progress}%" />
+	{/if}
+</div>
---
+++ src/routes/components/progressbars.svelte
@@ -0,0 +1,160 @@
+<script>
+	import DocSection from '$components/Docs/Section.svelte';
+	import Progressbar from '$components/Progressbar/Index.svelte';
+
+	let codeBlocks = [
+`
+<Progressbar progress="50" />
+`,
+
+`
+<div class="w-full">
+	<div class="mb-1 font-weight-bold">Small</div>
+	<Progressbar progress="50" size="h-1.5" />
+</div>
+
+<div class="w-full">
+	<div class="mb-1 font-weight-bold">Default</div>
+	<Progressbar progress="50" size="h-2.5" />
+</div>
+
+<div class="w-full">
+	<div class="mb-1 font-weight-bold">Large</div>
+	<Progressbar progress="50" size="h-4" />
+</div>
+
+<div class="w-full">
+	<div class="mb-1 font-weight-bold">Extra Large</div>
+	<Progressbar progress="50" size="h-6" />
+</div>
+`,
+
+`
+<Progressbar progress="50" size="h-4" labelInside />
+`,
+
+`
+<Progressbar progress="50" labelOutside="Flowbite-Svelte" />
+`,
+
+`
+<div class="w-full">
+	<div class="mb-1 text-secondary-700 font-weight-bold">Secondary</div>
+	<Progressbar progress="50" color="secondary" />
+</div>
+
+<div class="w-full">
+	<div class="mb-1 text-primary-700 font-weight-bold">Primary</div>
+	<Progressbar progress="50" color="primary" />
+</div>
+
+<div class="w-full">
+	<div class="mb-1 text-danger-700 font-weight-bold">Danger</div>
+	<Progressbar progress="50" color="danger" />
+</div>
+
+<div class="w-full">
+	<div class="mb-1 text-success-700 font-weight-bold">Success</div>
+	<Progressbar progress="50" color="success" />
+</div>
+
+<div class="w-full">
+	<div class="mb-1 text-warning-700 font-weight-bold">Warning</div>
+	<Progressbar progress="50" color="warning" />
+</div>
+
+<div class="w-full">
+	<div class="mb-1 text-indigo-700 font-weight-bold">Indigo</div>
+	<Progressbar progress="50" color="indigo" />
+</div>
+
+<div class="w-full">
+	<div class="mb-1 text-purple-700 font-weight-bold">Purple</div>
+	<Progressbar progress="50" color="purple" />
+</div>
+`,
+	];
+</script>
+
+<div class="pl-5 pr-5">
+	<DocSection class="mb-10" id="default-progress-bar" title="Default progress bar" code={codeBlocks[0]}>
+		<div slot="preview" class="flex flex-wrap items-center gap-2">
+			<Progressbar progress="50" />
+		</div>
+	</DocSection>
+
+	<DocSection class="mb-10" id="sizes" title="Sizes" code={codeBlocks[1]}>
+		<div slot="preview" class="flex flex-wrap items-center gap-3">
+			<div class="w-full">
+				<div class="mb-1 font-weight-bold">Small</div>
+				<Progressbar progress="50" size="h-1.5" />
+			</div>
+
+			<div class="w-full">
+				<div class="mb-1 font-weight-bold">Default</div>
+				<Progressbar progress="50" size="h-2.5" />
+			</div>
+
+			<div class="w-full">
+				<div class="mb-1 font-weight-bold">Large</div>
+				<Progressbar progress="50" size="h-4" />
+			</div>
+
+			<div class="w-full">
+				<div class="mb-1 font-weight-bold">Extra Large</div>
+				<Progressbar progress="50" size="h-6" />
+			</div>
+		</div>
+	</DocSection>
+	
+	<DocSection class="mb-10" id="with-label-inside" title="With label inside" code={codeBlocks[2]}>
+		<div slot="preview" class="flex flex-wrap items-center gap-2">
+			<Progressbar progress="50" size="h-4" labelInside />
+		</div>
+	</DocSection>
+	
+	<DocSection class="mb-10" id="with-label-outside" title="With label outside" code={codeBlocks[3]}>
+		<div slot="preview" class="flex flex-wrap items-center gap-2">
+			<Progressbar progress="50" labelOutside="Flowbite-Svelte" />
+		</div>
+	</DocSection>
+	
+	<DocSection class="mb-10" id="colors" title="Colors" code={codeBlocks[4]}>
+		<div slot="preview" class="flex flex-wrap items-center gap-3">
+			<div class="w-full">
+				<div class="mb-1 text-secondary-700 font-weight-bold">Secondary</div>
+				<Progressbar progress="50" color="secondary" />
+			</div>
+
+			<div class="w-full">
+				<div class="mb-1 text-primary-700 font-weight-bold">Primary</div>
+				<Progressbar progress="50" color="primary" />
+			</div>
+
+			<div class="w-full">
+				<div class="mb-1 text-danger-700 font-weight-bold">Danger</div>
+				<Progressbar progress="50" color="danger" />
+			</div>
+
+			<div class="w-full">
+				<div class="mb-1 text-success-700 font-weight-bold">Success</div>
+				<Progressbar progress="50" color="success" />
+			</div>
+
+			<div class="w-full">
+				<div class="mb-1 text-warning-700 font-weight-bold">Warning</div>
+				<Progressbar progress="50" color="warning" />
+			</div>
+
+			<div class="w-full">
+				<div class="mb-1 text-indigo-700 font-weight-bold">Indigo</div>
+				<Progressbar progress="50" color="indigo" />
+			</div>
+
+			<div class="w-full">
+				<div class="mb-1 text-purple-700 font-weight-bold">Purple</div>
+				<Progressbar progress="50" color="purple" />
+			</div>
+		</div>
+	</DocSection>
+</div>
