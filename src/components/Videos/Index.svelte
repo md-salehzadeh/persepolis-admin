52cfd2df5218c98ac72043b25432d81a82ffa7f9
md.salehzadeh <md.salehzadeh@gmail.com>
2023-01-31 18:29:55 +0330

    videos

--- src/components/Videos/Index.svelte
+++ src/components/Videos/Index.svelte
@@ -1,21 +1,23 @@
 <script>
-  import classNames from 'classnames';
+	import classNames from 'classnames';
 
-  export let src;
-  export let type = '';
-  export let trackSrc = '';
-  export let srclang = '';
-  export let label = '';
+	export let src;
+	export let type = '';
+	export let trackSrc = '';
+	export let srclang = '';
+	export let label = '';
 
-  let videoClass = classNames($$props.class);
+	let videoClass = classNames($$props.class);
 </script>
 
 <video {...$$restProps} class={videoClass}>
-  <source {src} {type} />
+	<source {src} {type} />
 
-  <slot />
+	<slot />
 
-  <track src={trackSrc} kind="captions" {srclang} {label} />
+	{#if trackSrc && label}
+		<track src={trackSrc} kind="captions" {srclang} {label} />
+	{/if}
 
-  Your browser does not support the video tag.
+	Your browser does not support the video tag.
 </video>
