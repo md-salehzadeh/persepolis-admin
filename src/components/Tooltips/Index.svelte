7c0d05bdd2af422e04e78b396efe046ede837bca
md.salehzadeh <md.salehzadeh@gmail.com>
2023-03-10 13:05:14 +0330

    avatar and tooltip

--- src/components/Tooltips/Index.svelte
+++ src/components/Tooltips/Index.svelte
@@ -1,181 +1,27 @@
 <script>
+	import Popper from '$lib/utils/Popper.svelte';
 	import classNames from 'classnames';
-	import { clickOutside } from '$lib/utils/clickOutside';
-	
-	import {
-		computePosition,
-		flip,
-		shift,
-		offset,
-		autoPlacement,
-		arrow as arrowFloat,
-	} from '@floating-ui/dom';
-	
-	import { onDestroy } from 'svelte';
 
-	export let placement = 'top';
-	export let trigger = 'hover';
-	export let theme = 'dark';
-	export let content = '';
-	export let animation = 'duration-300';
-	export let arrow = true;
-	export let tipClass = 'absolute inline-block rounded-lg py-2 px-3 text-sm font-medium shadow-sm';
-	export let tipColor = '';
+	export let style = 'dark';
 
-	const tipStyleClasses = {
+	export let defaultClass = 'py-2 px-3 text-sm font-medium';
+
+	const styles = {
 		dark: 'bg-gray-900 text-white dark:bg-gray-700',
 		light: 'border border-gray-200 bg-white text-gray-900',
-		auto: 'border border-gray-200 bg-white text-gray-900 dark:border-none dark:bg-gray-700 dark:text-white',
-		custom: tipColor
-	};
-
-	const arrowStyleClasses = {
-		dark: 'bg-gray-900 dark:bg-gray-700',
-		light: 'bg-white',
-		auto: 'bg-white dark:bg-gray-700',
-		custom: tipColor
+		auto: ' bg-white text-gray-900 dark:bg-gray-700 dark:text-white border border-gray-200 dark:border-0',
+		custom: '',
 	};
 
 	let toolTipClass;
-	
-	$: toolTipClass = classNames(
-		tipClass,
-		animation !== false && `transition-opacity ${animation}`,
-		!open && 'invisible opacity-0',
-		tipStyleClasses[theme],
-		$$props.class
-	);
-	
-	let open = false;
-	
-	const floatingPlacement = (placement) => {
-		return placement === 'auto' ? undefined : placement;
-	};
-
-	const floatingMiddleware = (arrowRef, placement) => {
-		const middleware = [];
-
-		middleware.push(offset(8));
-		middleware.push(placement === 'auto' ? autoPlacement() : flip());
-		middleware.push(shift({ padding: 8 }));
-
-		if (arrowRef) {
-			middleware.push(arrowFloat({ element: arrowRef }));
-		}
-		
-		return middleware;
-	};
-	
-	let placementData;
-	let tooltipRef, triggerRef, arrowRef;
-	
-	const updatePosition = () =>
-		computePosition(triggerRef, tooltipRef, {
-			middleware: floatingMiddleware(arrowRef, placement),
-			placement: floatingPlacement(placement)
-		}).then((data) => (placementData = data));
-
-	let attachedScroll = false;
-
-	$: tooltipRef && open && updatePosition();
 
 	$: {
-		if (open && !attachedScroll) {
-			attachedScroll = true;
-			window.addEventListener('scroll', updatePosition, true);
-		} else if (!open && attachedScroll) {
-			attachedScroll = false;
-			window.removeEventListener('scroll', updatePosition, true);
-		}
+		if ($$restProps.color) style = 'custom';
+		else $$restProps.color = 'none';
+		toolTipClass = classNames('tooltip', defaultClass, styles[style], $$props.class);
 	}
-	
-	onDestroy(() => {
-		if (attachedScroll) {
-			attachedScroll = false;
-			window.removeEventListener('scroll', updatePosition, true);
-		}
-	});
-
-	const arrPos = {
-		top: 'bottom',
-		right: 'left',
-		bottom: 'top',
-		left: 'right'
-	};
-
-	let floatingArrowPlacement;
-	
-	$: {
-		if (placementData) {
-			const arrow = placementData.middlewareData.arrow;
-			const pos = arrPos[placementData?.placement?.split('-')[0]] ?? 'top';
-			if (pos === 'top' || pos === 'bottom')
-				floatingArrowPlacement = `${pos}: -4px; left: ${px(arrow?.x)}`;
-			else floatingArrowPlacement = `${pos}: -4px; top: ${px(arrow?.y)}`;
-		}
-	}
-
-	const px = (x) => (x === undefined ? '' : x + 'px');
 </script>
 
-<svelte:window on:resize={() => open && updatePosition()} />
-
-<div
-	use:clickOutside={() => {
-		if (open) {
-			open = false;
-		}
-	}}
->
-	<div
-		class="w-fit"
-		
-		bind:this={triggerRef}
-		
-		on:mouseenter={() => {
-			if (trigger === 'hover') {
-				open = true;
-			}
-		}}
-
-		on:mouseleave={() => {
-			if (open && trigger === 'hover') {
-				open = false;
-			}
-		}}
-
-		on:click={() => {
-			if (trigger === 'click') {
-				open = !open;
-			}
-		}}
-	>
-		<slot />
-	</div>
-
-	<div
-		bind:this={tooltipRef}
-		data-testid="tooltip"
-		class={toolTipClass}
-		style:left={px(placementData?.x)}
-		style:top={px(placementData?.y)}
-		style:position={placementData?.strategy ?? ''}
-	>
-		<div class="relative z-20">
-			<slot name="content">
-				{content}
-			</slot>
-		</div>
-		
-		{#if arrow}
-			<div
-				class={classNames('absolute z-10 h-2 w-2 rotate-45', arrowStyleClasses[theme])}
-				data-testid="tooltip-arrow"
-				style={floatingArrowPlacement}
-				bind:this={arrowRef}
-			>
-				&nbsp;
-			</div>
-		{/if}
-	</div>
-</div>
+<Popper data-tooltip rounded shadow {...$$restProps} class={toolTipClass} on:show>
+	<slot />
+</Popper>
--- src/routes/components/avatars.svelte
+++ src/routes/components/avatars.svelte
@@ -2,184 +2,188 @@
 	import DocSection from '$components/Docs/Section.svelte';
 	import Avatar from '$components/Avatars/Index.svelte';
 	import Tooltip from '$components/Tooltips/Index.svelte';
+	import Dropdown from '$components/Dropdowns/Index.svelte';
+	import DropdownHeader from '$components/Dropdowns/DropdownHeader.svelte';
+	import DropdownDivider from '$components/Dropdowns/DropdownDivider.svelte';
+	import DropdownItem from '$components/Dropdowns/DropdownItem.svelte';
 
 	let codeBlocks = [
 `
-<Avatar src="https://i.pravatar.cc/300" rounded="md" />
+<Avatar src="https://i.pravatar.cc/300" />
 <Avatar src="https://i.pravatar.cc/300" rounded/>
 `,
 
 `
-<Avatar src="https://i.pravatar.cc/300" rounded="md" border />
-<Avatar src="https://i.pravatar.cc/300" rounded="md" border class="ring-danger-400 dark:ring-danger-300" />
+<Avatar src="https://i.pravatar.cc/300" border/>
+<Avatar src="https://i.pravatar.cc/300" border class="ring-danger-400 dark:ring-danger-300"/>
 `,
 
 `
-<Avatar rounded="md" />
-<Avatar rounded="full" />
-<Avatar rounded="md" border />
+<Avatar />
+<Avatar rounded />
+<Avatar border />
 <Avatar rounded border />
 `,
 
 `
-<Avatar rounded="md">JL</Avatar>
+<Avatar>JL</Avatar>
 `,
 
 `
-<Tooltip content="Jese Leos">
-	<Avatar rounded src="https://i.pravatar.cc/300"/>
-</Tooltip>
+<Avatar data-name="Jese Leos" rounded src="https://i.pravatar.cc/300"/>
+<Avatar data-name="Robert Gouth" rounded src="https://i.pravatar.cc/300"/>
+<Avatar data-name="Bonnie Green" rounded src="https://i.pravatar.cc/300"/>
 
-<Tooltip content="Jese Leos">
-	<Avatar rounded src="https://i.pravatar.cc/300"/>
-</Tooltip>
-
-<Tooltip content="Bonnie Green">
-	<Avatar rounded src="https://i.pravatar.cc/300"/>
-</Tooltip>
+<Tooltip triggeredBy="[data-name]" on:show={e => name = e.target.dataset.name}>{name}</Tooltip>
 `,
 
 `
-<Avatar src="https://i.pravatar.cc/300" dot={{top: true, color: "bg-danger-400"}}/>
-<Avatar	src="https://i.pravatar.cc/300" dot={{top: true, color: "bg-danger-400"}} rounded />
-<Avatar	src="https://i.pravatar.cc/300" dot={{color: "bg-green-400"}}/>
-<Avatar	src="https://i.pravatar.cc/300" dot={{color: "bg-green-400"}} rounded/>
+<Avatar src="https://i.pravatar.cc/300" dot={{color: "danger"}}/>
+<Avatar src="https://i.pravatar.cc/300" dot={{placement: "top-right", color: "danger"}} rounded />
+<Avatar src="https://i.pravatar.cc/300" dot={{placement: "bottom-right", color: "success"}} />
+<Avatar src="https://i.pravatar.cc/300" dot={{placement: "bottom-right"}} rounded />
+
+<Avatar dot={{}}/>
 `,
 
 `
 <div class="flex mb-5">
-	<Avatar src="https://i.pravatar.cc/300" rounded stacked />
-	<Avatar src="https://i.pravatar.cc/300" rounded stacked />
-	<Avatar src="https://i.pravatar.cc/300" rounded stacked />
-	<Avatar rounded stacked />
+	<Avatar src="https://i.pravatar.cc/300" stacked/>
+	<Avatar src="https://i.pravatar.cc/300" stacked />
+	<Avatar src="https://i.pravatar.cc/300" stacked />
+	<Avatar stacked />
 </div>
 
 <div class="flex">
-	<Avatar src="https://i.pravatar.cc/300" rounded stacked />
-	<Avatar src="https://i.pravatar.cc/300" rounded stacked />
-	<Avatar src="https://i.pravatar.cc/300" rounded stacked />
-	<Avatar rounded stacked href="/" class="text-white bg-gray-700 dark:bg-gray-700 hover:bg-gray-600">+99</Avatar>
+	<Avatar src="https://i.pravatar.cc/300" stacked />
+	<Avatar src="https://i.pravatar.cc/300" stacked />
+	<Avatar src="https://i.pravatar.cc/300" stacked />
+	<Avatar stacked href="/" class="bg-gray-700 text-white hover:bg-gray-600 text-sm">+99</Avatar>
 </div>
 `,
 
 `
-<Avatar src="https://i.pravatar.cc/300" rounded="lg" />
+<div class="flex items-center space-x-4">
+	<Avatar src="https://i.pravatar.cc/300" rounded/>
 
-<div class="space-y-1 font-medium dark:text-white">
-	<div>Jese Leos</div>
-	<div class="text-sm text-gray-500 dark:text-gray-400">Joined in August 2014</div>
+	<div class="space-y-1 font-medium dark:text-white">
+		<div>Jese Leos</div>
+
+		<div class="text-sm text-gray-500 dark:text-gray-400">Joined in August 2014</div>
+	</div>
 </div>
 `,
 
 `
-<Dropdown>
-	<Avatar slot="trigger" src="https://i.pravatar.cc/300" dot={{color:'bg-green-400'}} />
+<Avatar id="user-drop" src="https://i.pravatar.cc/300" dot={{color:'green'}} />
 
+<Dropdown triggeredBy="#user-drop">
 	<DropdownHeader>
 		<span class="block text-sm"> Bonnie Green </span>
-		<span class="block text-sm font-medium truncate"> name@flowbite.com </span>
+		<span class="block truncate text-sm font-medium"> name@flowbite.com </span>
 	</DropdownHeader>
 
 	<DropdownItem>Dashboard</DropdownItem>
+
 	<DropdownItem>Settings</DropdownItem>
+
 	<DropdownItem>Earnings</DropdownItem>
 
 	<DropdownDivider />
-	
+
 	<DropdownItem>Sign out</DropdownItem>
 </Dropdown>
 `,
 
 `
-<Avatar src="https://i.pravatar.cc/300" rounded="lg" size="xs" />
-<Avatar src="https://i.pravatar.cc/300" rounded="lg" size="sm" />
-<Avatar src="https://i.pravatar.cc/300" rounded="lg" size="md" />
-<Avatar src="https://i.pravatar.cc/300" rounded="lg" size="lg" />
-<Avatar src="https://i.pravatar.cc/300" rounded="lg" size="xl" />
+<div class="flex flex-wrap justify-center space-x-4">
+	<Avatar src="https://i.pravatar.cc/300" rounded size="xs" />
+	<Avatar src="https://i.pravatar.cc/300" rounded size="sm" />
+	<Avatar src="https://i.pravatar.cc/300" rounded size="md" />
+	<Avatar src="https://i.pravatar.cc/300" rounded size="lg" />
+	<Avatar src="https://i.pravatar.cc/300" rounded size="xl" />
+</div>
 `,
 	];
 </script>
 
 <div class="pl-5 pr-5">
 	<DocSection class="mb-10" id="default-avatar" title="Default avatar" code={codeBlocks[0]}>
-		<div slot="preview" class="flex flex-wrap items-center gap-2">
-			<Avatar src="https://i.pravatar.cc/300" rounded="md" />
+		<div slot="preview" class="flex flex-wrap items-center gap-4">
+			<Avatar src="https://i.pravatar.cc/300" />
 	 		<Avatar src="https://i.pravatar.cc/300" rounded/>
 		</div>
 	</DocSection>
 
 	<DocSection class="mb-10" id="bordered" title="Bordered" code={codeBlocks[1]}>
-		<div slot="preview" class="flex flex-wrap items-center gap-2">
-			<Avatar src="https://i.pravatar.cc/300" rounded="md" border />
-			<Avatar src="https://i.pravatar.cc/300" rounded="md" border class="ring-danger-400 dark:ring-danger-300" />
+		<div slot="preview" class="flex flex-wrap items-center gap-4">
+			<Avatar src="https://i.pravatar.cc/300" border/>
+			<Avatar src="https://i.pravatar.cc/300" border class="ring-danger-400 dark:ring-danger-300"/>
 		</div>
 	</DocSection>
 	
 	<DocSection class="mb-10" id="placeholder" title="Placeholder" code={codeBlocks[2]}>
-		<div slot="preview" class="flex flex-wrap items-center gap-2">
-			<Avatar rounded="md" />
-			<Avatar rounded="full" />
-			<Avatar rounded="md" border />
+		<div slot="preview" class="flex flex-wrap items-center gap-4">
+			<Avatar />
+			<Avatar rounded />
+			<Avatar border />
 			<Avatar rounded border />
 		</div>
 	</DocSection>
 	
 	<DocSection class="mb-10" id="placeholder-initials" title="Placeholder initials" code={codeBlocks[3]}>
-		<div slot="preview" class="flex flex-wrap items-center gap-2">
-			<Avatar rounded="md">JL</Avatar>
+		<div slot="preview" class="flex flex-wrap items-center gap-4">
+			<Avatar>JL</Avatar>
 		</div>
 	</DocSection>
 	
 	<DocSection class="mb-10" id="avatar-tooltip" title="Avatar tooltip" code={codeBlocks[4]}>
-		<div slot="preview" class="flex flex-wrap items-center gap-2">
-			<Tooltip content="Jese Leos">
-				<Avatar rounded src="https://i.pravatar.cc/300"/>
-			</Tooltip>
-			
-			<Tooltip content="Jese Leos">
-				<Avatar rounded src="https://i.pravatar.cc/300"/>
-			</Tooltip>
+		<div slot="preview" class="flex flex-wrap items-center gap-4">
+			<Avatar data-name="Jese Leos" rounded src="https://i.pravatar.cc/300"/>
+			<Avatar data-name="Robert Gouth" rounded src="https://i.pravatar.cc/300"/>
+			<Avatar data-name="Bonnie Green" rounded src="https://i.pravatar.cc/300"/>
 
-			<Tooltip content="Bonnie Green">
-				<Avatar rounded src="https://i.pravatar.cc/300"/>
-			</Tooltip>
+			<Tooltip triggeredBy="[data-name]" on:show={e => name = e.target.dataset.name}>{name}</Tooltip>
 		</div>
 	</DocSection>
 	
 	<DocSection class="mb-10" id="dot-indicator" title="Dot indicator" code={codeBlocks[5]}>
-		<div slot="preview" class="flex flex-wrap items-center gap-2">
-			<Avatar src="https://i.pravatar.cc/300" dot={{top: true, color: "bg-danger-400"}} rounded="lg" />
-			<Avatar	src="https://i.pravatar.cc/300" dot={{top: true, color: "bg-danger-400"}} rounded />
-			<Avatar	src="https://i.pravatar.cc/300" dot={{color: "bg-green-400"}} rounded="lg" />
-			<Avatar	src="https://i.pravatar.cc/300" dot={{color: "bg-green-400"}} rounded />
+		<div slot="preview" class="flex flex-wrap items-center gap-4">
+			<Avatar src="https://i.pravatar.cc/300" dot={{color: "danger"}}/>
+			<Avatar src="https://i.pravatar.cc/300" dot={{placement: "top-right", color: "danger"}} rounded />
+			<Avatar src="https://i.pravatar.cc/300" dot={{placement: "bottom-right", color: "success"}} />
+			<Avatar src="https://i.pravatar.cc/300" dot={{placement: "bottom-right"}} rounded />
+
+			<Avatar dot={{}}/>
 		</div>
 	</DocSection>
 	
 	<DocSection class="mb-10" id="stacked" title="Stacked" code={codeBlocks[6]}>
 		<div slot="preview" class="pl-3.5">
 			<div class="flex mb-5">
-				<Avatar src="https://i.pravatar.cc/300" rounded stacked />
-				<Avatar src="https://i.pravatar.cc/300" rounded stacked />
-				<Avatar src="https://i.pravatar.cc/300" rounded stacked />
-				<Avatar rounded stacked />
+				<Avatar src="https://i.pravatar.cc/300" stacked/>
+				<Avatar src="https://i.pravatar.cc/300" stacked />
+				<Avatar src="https://i.pravatar.cc/300" stacked />
+				<Avatar stacked />
 			</div>
-			
+
 			<div class="flex">
-				<Avatar src="https://i.pravatar.cc/300" rounded stacked />
-				<Avatar src="https://i.pravatar.cc/300" rounded stacked />
-				<Avatar src="https://i.pravatar.cc/300" rounded stacked />
-				<Avatar rounded stacked href="/" class="text-white bg-gray-700 dark:bg-gray-700 hover:bg-gray-600">+99</Avatar>
+				<Avatar src="https://i.pravatar.cc/300" stacked />
+				<Avatar src="https://i.pravatar.cc/300" stacked />
+				<Avatar src="https://i.pravatar.cc/300" stacked />
+				<Avatar stacked href="/" class="bg-gray-700 text-white hover:bg-gray-600 text-sm">+99</Avatar>
 			</div>
 		</div>
 	</DocSection>
 	
 	<DocSection class="mb-10" id="avatar-text" title="Avatar text" code={codeBlocks[7]}>
-		<div slot="preview" class="flex flex-wrap items-center gap-2">
+		<div slot="preview">
 			<div class="flex items-center space-x-4">
-				<Avatar src="https://i.pravatar.cc/300" rounded="lg" />
+				<Avatar src="https://i.pravatar.cc/300" rounded/>
 
 				<div class="space-y-1 font-medium dark:text-white">
 					<div>Jese Leos</div>
+
 					<div class="text-sm text-gray-500 dark:text-gray-400">Joined in August 2014</div>
 				</div>
 			</div>
@@ -188,29 +192,36 @@
 	
 	<DocSection class="mb-10" id="user-dropdown" title="User dropdown" code={codeBlocks[8]}>
 		<div slot="preview" class="flex flex-wrap items-center gap-2">
-			Dropdown
-			<!-- <Dropdown>
-				<Avatar slot="trigger" src="https://i.pravatar.cc/300" dot={{color:'bg-green-400'}} />
+			<Avatar id="user-drop" src="https://i.pravatar.cc/300" dot={{color:'green'}} />
+
+			<Dropdown triggeredBy="#user-drop">
 				<DropdownHeader>
-				  <span class="block text-sm"> Bonnie Green </span>
-				  <span class="block text-sm font-medium truncate"> name@flowbite.com </span>
+					<span class="block text-sm"> Bonnie Green </span>
+					<span class="block truncate text-sm font-medium"> name@flowbite.com </span>
 				</DropdownHeader>
+
 				<DropdownItem>Dashboard</DropdownItem>
+
 				<DropdownItem>Settings</DropdownItem>
+
 				<DropdownItem>Earnings</DropdownItem>
+
 				<DropdownDivider />
+
 				<DropdownItem>Sign out</DropdownItem>
-			</Dropdown> -->
+			</Dropdown>
 		</div>
 	</DocSection>
 	
 	<DocSection class="mb-10" id="sizes" title="Sizes" code={codeBlocks[9]}>
-		<div slot="preview" class="flex flex-wrap items-center gap-2">
-			<Avatar src="https://i.pravatar.cc/300" rounded="lg" size="xs" />
-			<Avatar src="https://i.pravatar.cc/300" rounded="lg" size="sm" />
-			<Avatar src="https://i.pravatar.cc/300" rounded="lg" size="md" />
-			<Avatar src="https://i.pravatar.cc/300" rounded="lg" size="lg" />
-			<Avatar src="https://i.pravatar.cc/300" rounded="lg" size="xl" />
+		<div slot="preview">
+			<div class="flex flex-wrap justify-center space-x-4">
+				<Avatar src="https://i.pravatar.cc/300" rounded size="xs" />
+				<Avatar src="https://i.pravatar.cc/300" rounded size="sm" />
+				<Avatar src="https://i.pravatar.cc/300" rounded size="md" />
+				<Avatar src="https://i.pravatar.cc/300" rounded size="lg" />
+				<Avatar src="https://i.pravatar.cc/300" rounded size="xl" />
+			</div>
 		</div>
 	</DocSection>
 </div>
